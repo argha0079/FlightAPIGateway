f8b57b68f79fa641fa4b225a5be5f460809facc9
Argha Nath <argha2489@gmail.com>
2026-09-20 17:14:59 +0530

    booking proxy setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 const PORT = 3005;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('combined'));
+
+app.use("/bookingservice", createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true }))
+
 app.get("/home", (req, res) => {
     res.json({message: 'OK'});
-})
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-})
+});
 
