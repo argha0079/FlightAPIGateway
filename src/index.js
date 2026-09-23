f7529b63de74f8346bb1a6763f1a2d63bb650b85
Argha Nath <argha2489@gmail.com>
2026-09-23 22:48:57 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
-
+import { rateLimit } from "express-rate-limit";
 const app = express();
 const PORT = 3005;
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 3,
+})
+
+app.use(morgan('combined'));
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(morgan('combined'));
+
 
 app.use("/bookingservice", createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true }))
 
